23c6d37e6955759802c669ea2fd97e0c2d9f60ac
DELL <DELL@DESKTOP-0R4V30I>
2024-08-02 12:02:03 +0530

    2nd commit

---
+++ src/Object_Repo/happy.java
@@ -0,0 +1,5 @@
+package Object_Repo;
+
+public class happy {
+
+}
--- src/testcase_reposi/ORG_02.java
+++ src/testcase_reposi/ORG_02.java
@@ -1,5 +1,19 @@
 package testcase_reposi;
 
-public class ORG_02 {
+import genericlib.ExcelUtilityorLib;
+import genericlib.JavaLibrary;
 
+public class ORG_02 
+{
+	public void ORG_02() throws Exception
+	{
+		JavaLibrary JUTIL = new JavaLibrary();
+		int num = JUTIL.generateRandomNumber(1000);
+		ExcelUtilityorLib  EUTIL = new ExcelUtilityorLib ();
+		String ORGNAME = EUTIL.ReadingDataFromExcel("Oganization",5,1);
+		String ORGNAME = EUTIL.ReadingDataFromExcel("Oganization",5,1);
+		String ORGNAME = EUTIL.ReadingDataFromExcel("Oganization",5,1);
+		String ORGNAME = EUTIL.ReadingDataFromExcel("Oganization",5,1);
+		String ORGNAME = EUTIL.ReadingDataFromExcel("Oganization",5,1);
+	}
 }
